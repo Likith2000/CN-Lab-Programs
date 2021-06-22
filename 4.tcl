be69cd31eb015e0fba5935c911f85c85aa7bed8d
Likith <likithsrinath2000@gmail.com>
2021-06-22 11:27:24 +0530

    Update 4.tcl

--- 4.tcl
+++ 4.tcl
@@ -88,20 +88,3 @@ $ns at $val(stop) "$ns nam-end-wireless $val(stop)"
 $ns at $val(stop) "finish"
 $ns at $val(stop) "puts \"done\" ; $ns halt"
 $ns run
-
-
-BEGIN{
-	PacketRcvd=0;
-	Throughput=0.0;
-}
-{
-	if(($1=="r")&&($3=="_5_")&&($4=="AGT")&&($7=="tcp")&&($8>1000))
-	{
-		PacketRcvd++;
-	}
-}
-END {
-	Throughput=((PacketRcvd*1500*8)/(99.0*1000000));
-	printf "the throughput is:%f\n",Throughput;
-}
-
